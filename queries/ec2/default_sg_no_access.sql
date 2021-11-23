c668a580ec7b39680a18d93acaf2d98d86ce670b
bbernays <ben@bernays.me>
2021-11-23 13:47:16 -0500

    Update default_sg_no_access.sql

--- queries/ec2/default_sg_no_access.sql
+++ queries/ec2/default_sg_no_access.sql
@@ -4,4 +4,4 @@ from
     aws_ec2_security_groups
     JOIN aws_ec2_security_group_ip_permissions ON aws_ec2_security_groups.cq_id = aws_ec2_security_group_ip_permissions.security_group_cq_id
 where
-    group_name = 'default' 
+    group_name = 'default'  
