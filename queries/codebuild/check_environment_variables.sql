10fc3b4ead51231c2536eae362f30cfd63f2f7d1
Andrii Romanenko <andrejjrom@gmail.com>
2021-11-24 16:05:08 +0200

    control rollback

--- queries/codebuild/check_environment_variables.sql
+++ queries/codebuild/check_environment_variables.sql
@@ -1,6 +1,6 @@
-SELECT DISTINCT account_id, region, arn, name
-FROM aws_codebuild_projects
-WHERE (environment_variables -> 'AWS_ACCESS_KEY_ID' IS NOT NULL
-	OR environment_variables -> 'AWS_SECRET_ACCESS_KEY' IS NOT NULL)
---This control is not supported in the regions below
-AND region NOT IN ('us-gov-east-1', 'us-gov-west-1', 'eu-south-1', 'ap-northeast-3', 'af-south-1'); 
+SELECT DISTINCT p.account_id, p.region, p.name
+FROM aws_codebuild_projects p
+JOIN aws_codebuild_project_environment_variables e ON
+p.cq_id = e.project_cq_id
+WHERE (e."name" = 'AWS_ACCESS_KEY_ID'
+	OR e."name" = 'AWS_SECRET_ACCESS_KEY');
