7dced0c3800ae88fc6e492397281f346b63999fb
bbernays <ben@bernays.me>
2021-11-24 16:15:39 -0500

    Update api_gw_execution_logging_enabled.sql

--- queries/apigateway/api_gw_execution_logging_enabled.sql
+++ queries/apigateway/api_gw_execution_logging_enabled.sql
@@ -1,4 +1,6 @@
-select
+select rest_api_cq_id 
+
+from (select
     cq_id,
     stage_name,
     rest_api_cq_id,
@@ -16,4 +18,12 @@ from
     aws_apigateway_rest_api_stages,
     jsonb_each_text(aws_apigateway_rest_api_stages.method_settings)
     where (value::json -> 'LoggingLevel')::TEXT not in ('ERROR','INFO')
-order by cq_id, stage_name, method;
+order by cq_id, stage_name, method) as t
+
+union
+
+
+
+
+
+select api_cq_id from aws_apigatewayv2_api_stages where route_settings_logging_level in (NULL, 'OFF')
