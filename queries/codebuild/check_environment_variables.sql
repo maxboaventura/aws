c1ba28a29707fcee849e2a29100966a1988c8c5b
Andrii Romanenko <andrejjrom@gmail.com>
2021-11-24 21:07:13 +0200

    query adjusted

--- queries/codebuild/check_environment_variables.sql
+++ queries/codebuild/check_environment_variables.sql
@@ -3,5 +3,6 @@ FROM aws_codebuild_projects p
 JOIN aws_codebuild_project_environment_variables e ON
 p.cq_id = e.project_cq_id
 WHERE e."type" = 'PLAINTEXT'
-AND (e."name" = 'AWS_ACCESS_KEY_ID'
-	OR e."name" = 'AWS_SECRET_ACCESS_KEY');
+AND (UPPER(e.name) LIKE '%ACCESS_KEY%'
+	OR UPPER(e.name) LIKE '%SECRET%'
+		OR UPPER(e.name) LIKE '%PASSWORD%');
