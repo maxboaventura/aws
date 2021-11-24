09640957ffa91adb4be75208b61530ba82d60cac
Andrii Romanenko <andrejjrom@gmail.com>
2021-11-24 16:13:42 +0200

    chech adjusted to latest structure

--- queries/codebuild/check_environment_variables.sql
+++ queries/codebuild/check_environment_variables.sql
@@ -2,5 +2,6 @@ SELECT DISTINCT p.account_id, p.region, p.name
 FROM aws_codebuild_projects p
 JOIN aws_codebuild_project_environment_variables e ON
 p.cq_id = e.project_cq_id
-WHERE (e."name" = 'AWS_ACCESS_KEY_ID'
+WHERE e."type" = 'PLAINTEXT'
+AND (e."name" = 'AWS_ACCESS_KEY_ID'
 	OR e."name" = 'AWS_SECRET_ACCESS_KEY');
