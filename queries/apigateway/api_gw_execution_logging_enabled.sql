0549c435347ab8609c7f9f7590396670dff2a787
bbernays <ben@bernays.me>
2021-11-26 07:44:32 -0500

    Update api_gw_execution_logging_enabled.sql

--- queries/apigateway/api_gw_execution_logging_enabled.sql
+++ queries/apigateway/api_gw_execution_logging_enabled.sql
@@ -1,29 +1,18 @@
-select rest_api_cq_id 
-
-from (select
-    cq_id,
-    stage_name,
-    rest_api_cq_id,
-    tracing_enabled as stage_data_trace_enabled,
-    cache_cluster_enabled as stage_caching_enabled,
-    web_acl_arn as waf,
-    client_certificate_id as cert,
-    key as method,
-    (value::json -> 'DataTraceEnabled')::TEXT::BOOLEAN as data_trace_enabled,
-    (value::json ->'CachingEnabled')::TEXT::BOOLEAN as caching_enabled,
-    (value::json -> 'CacheDataEncrypted')::TEXT::BOOLEAN as cache_data_encrypted,
-    (value::json -> 'LoggingLevel')::TEXT as logging_level
-    
-from
-    aws_apigateway_rest_api_stages,
-    jsonb_each_text(aws_apigateway_rest_api_stages.method_settings)
-    where (value::json -> 'LoggingLevel')::TEXT not in ('ERROR','INFO')
-order by cq_id, stage_name, method) as t
-
-union
-select
+SELECT
+    rest_api_cq_id
+FROM
+    (
+        SELECT
+            *
+        FROM
+            api_gateway_method_settings
+        WHERE
+            logging_level NOT IN ('ERROR', 'INFO')
+    ) as t
+UNION
+SELECT
     api_cq_id
-from
+FROM
     aws_apigatewayv2_api_stages
-where
-    route_settings_logging_level in (NULL, 'OFF')
+WHERE
+    route_settings_logging_level IN (NULL, 'OFF')
