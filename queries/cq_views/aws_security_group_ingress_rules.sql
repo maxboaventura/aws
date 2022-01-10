ab71e287d1ed4b5482a7c5c3c28bf1ca02fb9130
bbernays <ben@cloudquery.io>
2022-01-10 03:15:38 -0500

    Change Name of Field (#77)
    * Update subnets_that_assign_public_ips.sql
    
    * Update policy.hcl
    
    Co-authored-by: Ron <38083777+roneli@users.noreply.github.com>

--- queries/cq_views/aws_security_group_ingress_rules.sql
+++ queries/cq_views/aws_security_group_ingress_rules.sql
@@ -1,21 +1,16 @@
 WITH sg_rules_ports AS (SELECT sg.account_id,
-                               sg.region,
-                               sg.group_name,
-                               sg.arn,
-                               sg.id,
-                               p.from_port,
-                               p.to_port,
-                               p.ip_protocol,
-                               p.cq_id AS permission_id
-                        FROM aws_ec2_security_groups sg
-                                 LEFT JOIN aws_ec2_security_group_ip_permissions p
-                                           ON sg.cq_id = p.security_group_cq_id)
-SELECT sgs.*, r.cidr_ip AS ip
+			sg.region,
+			sg.group_name,
+			sg.arn,
+			sg.id,
+			p.from_port,
+			p.to_port,
+			p.ip_protocol,
+			p.cq_id AS permission_id
+		FROM aws_ec2_security_groups sg
+		LEFT JOIN aws_ec2_security_group_ip_permissions p ON sg.cq_id = p.security_group_cq_id
+		WHERE p.permission_type = 'ingress' )
+SELECT sgs.*,
+	r.cidr AS ip
 FROM sg_rules_ports sgs
-         LEFT JOIN aws_ec2_security_group_ip_permission_ip_ranges r
-                   ON sgs.permission_id = r.security_group_ip_permission_cq_id
-UNION
-SELECT sgs.*, r.cidr_ipv6 AS ip
-FROM sg_rules_ports sgs
-         LEFT JOIN aws_ec2_security_group_ip_permission_ipv6_ranges r
-                   ON sgs.permission_id = r.security_group_ip_permission_cq_id
+LEFT JOIN aws_ec2_security_group_ip_permission_ip_ranges r on sgs.permission_id = r.security_group_ip_permission_cq_id
