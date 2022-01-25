22bb9c218c56f2f22a2a5b2f641e91d8e86b0fd1
Andrii Romanenko <andrejjrom@gmail.com>
2022-01-25 10:21:03 +0200

    linter

--- queries/s3/restrict_cross_account_actions.sql
+++ queries/s3/restrict_cross_account_actions.sql
@@ -1,52 +1,53 @@
 SELECT arn,
+       account_id,
+       a.value::TEXT AS ACTION,
+       p.value::TEXT AS USER --noqa
+FROM (SELECT aws_s3_buckets.arn,
     account_id,
-    a.value::TEXT AS action,
-    p.value::TEXT AS user --noqa
-FROM
-    (SELECT aws_s3_buckets.arn,
-            account_id,
-            -- For each Statement return an array containing the prinicipals
-            CASE
-                WHEN
-                    JSONB_TYPEOF(
-                        statements -> 'Principal'
-                    ) = 'string' THEN JSONB_BUILD_ARRAY(
-                        statements -> 'Principal'
-                    )
-                WHEN
-                    JSONB_TYPEOF(
-                        statements -> 'Principal' -> 'AWS'
-                    ) = 'string' THEN JSONB_BUILD_ARRAY(
-                        statements -> 'Principal' -> 'AWS'
-                    )
-                WHEN
-                    JSONB_TYPEOF(
-                        statements -> 'Principal' -> 'AWS'
-                    ) = 'array' THEN statements -> 'Principal' -> 'AWS'
-           END AS principals,
-            -- For each Statement return an array containing the Actions
-            CASE
-                WHEN
-                    JSONB_TYPEOF(
-                        statements -> 'Action'
-                    ) = 'string' THEN JSONB_BUILD_ARRAY(statements -> 'Action')
-                WHEN
-                    JSONB_TYPEOF(
-                        statements -> 'Action'
-                    ) = 'array' THEN statements -> 'Action'
-           END AS actions
-        FROM aws_s3_buckets,
-            JSONB_ARRAY_ELEMENTS(policy -> 'Statement') AS statements
-        WHERE statements -> 'Effect' = '"Allow"' ) AS flatten_statements,
+    -- For each Statement return an array containing the prinicipals
+    CASE
+    WHEN
+    JSONB_TYPEOF(
+    statements -> 'Principal'
+    ) = 'string' THEN JSONB_BUILD_ARRAY(
+    statements -> 'Principal'
+    )
+    WHEN
+    JSONB_TYPEOF(
+    statements -> 'Principal' -> 'AWS'
+    ) = 'string' THEN JSONB_BUILD_ARRAY(
+    statements -> 'Principal' -> 'AWS'
+    )
+    WHEN
+    JSONB_TYPEOF(
+    statements -> 'Principal' -> 'AWS'
+    ) = 'array' THEN statements -> 'Principal' -> 'AWS'
+    END AS principals,
+    -- For each Statement return an array containing the Actions
+    CASE
+    WHEN
+    JSONB_TYPEOF(
+    statements -> 'Action'
+    ) = 'string' THEN JSONB_BUILD_ARRAY(statements -> 'Action')
+    WHEN
+    JSONB_TYPEOF(
+    statements -> 'Action'
+    ) = 'array' THEN statements -> 'Action'
+    END AS actions
+    FROM aws_s3_buckets,
+    JSONB_ARRAY_ELEMENTS(policy -> 'Statement') AS statements
+    WHERE statements -> 'Effect' = '"Allow"') AS flatten_statements,
     JSONB_ARRAY_ELEMENTS(TO_JSONB(actions)) AS a,
     JSONB_ARRAY_ELEMENTS(TO_JSONB(principals)) AS p
 WHERE
-    -- Any cross account prinicipals (or unknown principals) get flagged
+-- Any cross account prinicipals (or unknown principals) get flagged
     (
-        p.value::TEXT NOT LIKE '"arn:aws:iam::' || account_id || ':%"' OR p.value::TEXT = '"*"'
+    p.value::TEXT NOT LIKE '"arn:aws:iam::' || account_id || ':%"'
+   OR p.value::TEXT = '"*"'
     )
-    -- Any broad permissions or Deletes get flagged
-    AND (a.value::TEXT LIKE '"s3:%*"' OR a.value::TEXT LIKE '"s3:DeleteObject"')
+-- Any broad permissions or Deletes get flagged
+  AND (a.value::TEXT LIKE '"s3:%*"'
+   OR a.value::TEXT LIKE '"s3:DeleteObject"')
 
 -- This will flag ALL canoninical IDs as NOT COMPLIANT
 -- This will flag ALL users that have been deleted as NOT COMPLIANT
--- queries/secretsmanager/remove_unused_secrets_manager_secrets.sql
+++ queries/secretsmanager/remove_unused_secrets_manager_secrets.sql
@@ -1,7 +1,10 @@
-ELECT account_id,
+SELECT account_id,
        region,
        arn,
        name
 FROM aws_secretsmanager_secrets
-WHERE last_accessed_date IS NULL AND created_date > now() - INTERVAL '90 days'
-     OR last_accessed_date IS NOT NULL AND last_accessed_date > now() - INTERVAL '90 days';
+WHERE last_accessed_date IS NULL
+    AND created_date > NOW() - INTERVAL '90 days'
+   OR last_accessed_date IS NOT NULL
+    AND last_accessed_date
+          > NOW() - INTERVAL '90 days';
