fbce4d4162059a73f64e4f1972c54748fe4fd023
Itay Zagron <zagron@gmail.com>
2022-05-10 09:53:53 +0300

    fix: Update waf / wafv2 logging configuration queries

--- queries/waf/waf_web_acl_logging_should_be_enabled.sql
+++ queries/waf/waf_web_acl_logging_should_be_enabled.sql
@@ -1,6 +1,6 @@
 -- WAF Classic
-SELECT account_id,
-       arn
-FROM aws_waf_web_acls
-WHERE logging_configuration IS NULL
-      OR logging_configuration = '{}';
+SELECT ac.account_id,
+       ac.arn
+FROM aws_waf_web_acls ac
+LEFT JOIN aws_waf_web_acl_logging_configuration lg ON ac.cq_id = lg.web_acl_cq_id
+WHERE lg.web_acl_cq_id IS NULL;
--- queries/wafv2/wafv2_web_acl_logging_should_be_enabled.sql
+++ queries/wafv2/wafv2_web_acl_logging_should_be_enabled.sql
@@ -1,14 +1,15 @@
 -- WAF Classic
-SELECT account_id,
-       arn
-FROM aws_waf_web_acls
-WHERE logging_configuration IS NULL
-      OR logging_configuration = '{}'
+SELECT ac.account_id,
+       ac.arn
+FROM aws_waf_web_acls ac
+         LEFT JOIN aws_waf_web_acl_logging_configuration lg ON ac.cq_id = lg.web_acl_cq_id
+WHERE lg.web_acl_cq_id IS NULL
 
 UNION
+
 -- WAF V2
-SELECT account_id,
-       arn
-FROM aws_wafv2_web_acls
-WHERE logging_configuration IS NULL
-      OR logging_configuration = '{}';
+SELECT ac.account_id,
+       ac.arn
+FROM aws_wafv2_web_acls ac
+         LEFT JOIN aws_wafv2_web_acl_logging_configuration lg ON ac.cq_id = lg.web_acl_cq_id
+WHERE lg.web_acl_cq_id IS NULL;
